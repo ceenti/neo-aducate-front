dd091dfa4467ff4bb988152ee73fe2016cfb6f27
Cinthia Sánchez <ceenti@gmail.com>
2021-02-21 01:23:05 -0600

    Add changes on dashboard styles and form

--- src/Components/Dashboard/SchoolForm.js
+++ src/Components/Dashboard/SchoolForm.js
@@ -1,27 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
 
 function SchoolForm() {
   return (
     <div className="school-form">
       <h2>Nueva Escuela</h2>
-      <form>
-        <div>
-          <label for="name">Nombre:</label>
-          <input className="col-6" type="text" id="name" name="name" placeholder="School Name" />
-        </div>
-
-        <div>
-          <label for="address">Dirección:</label>
-          <input className="col-6" type="text" id="address" name="address" placeholder="Address" />
-        </div>
-
-        <div>
-          <label for="phone">Teléfono:</label>
-          <input className="col-6" type="text" id="phone" name="phone" placeholder="(55) 5555-5555" />
-        </div>
-
-        <input className="btn btn-primary" type="submit" id="submit" name="submit" value="Create School" />
-      </form>
+      <Form className="school-form">
+      <FormGroup>
+        <Label for="school_name">Nombre de la escuela:</Label>
+        <Input type="text" name="school_name" id="school_name" placeholder="with a placeholder" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="address">Ubicación</Label>
+        <Input type="text" name="address" id="address" placeholder="123 Fake Street st" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="phone">Teléfono</Label>
+        <Input type="text" name="phone" id="phone" placeholder="55-5555-5555" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="name">Nombre de profesor</Label>
+        <Input type="text" name="name" id="name" placeholder="John Doe" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="email">Email</Label>
+        <Input type="email" name="email" id="email" placeholder="email@domain.com" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input type="password" name="password" id="examplePassword" placeholder="password" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleSelect">Role</Label>
+        <Input type="select" name="role" id="exampleSelect">
+          <option>School</option>
+          <option>Student</option>
+        </Input>
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleSelect">Tier</Label>
+        <Input type="select" name="tier" id="exampleSelect">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </Input>
+      </FormGroup>
+     
+      <Button className="btn-pink">Submit</Button>
+    </Form>  
     </div>
   )
 }
